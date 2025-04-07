440f9a393f63759fc313037e2767fce76f164adf
Tamarazoubi <tamarazoubi2021@gmail.com>
2025-04-07 23:39:19 +0300

    add

---
+++ lib/Form2.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_track/stack.dart';
+//import 'package:lottie/lottie.dart';
+
+
+
+class Rigistration extends StatefulWidget {
+  const Rigistration({super.key});
+
+  @override
+  State<Rigistration> createState() => _RigistrationState();
+}
+
+class _RigistrationState extends State<Rigistration> {
+  final formkey=GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(child:
+
+      Padding(
+        padding: const EdgeInsets.all(10),
+        child: Form(
+            key: formkey,
+            child:
+            SingleChildScrollView(
+              child: Column(
+                children: [
+                  //  Image.network('https://ouch-prod-var-cdn.icons8.com/ob/illustrations/thumbs/WRHAp9Sia49MVfiY.webp'),
+                 // Lottie.network('https://lottie.host/4d0b2829-19d3-42a3-b7b5-8fb6b35c361e/JWzDLc1WTe.json'),
+                  TextFormField(
+                    decoration: InputDecoration(
+                        label: Text("Email"),
+
+                        hintText: "example@domain.com",
+                        prefixIcon: Icon(Icons.email),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.blueAccent,
+                            width: 3,
+                          ),
+
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.blueAccent,
+                            width: 3,
+                          ),
+                        )
+                    ),
+                    validator: (email){
+                      if(!(checkemail(email!))){
+                        return 'not vaild';
+                      }
+                      else
+                        return 'vaild';
+                    },
+                  ),
+                  SizedBox(height: 30,),
+                  TextFormField(
+                    validator: (passward){
+                      if(!(checkpassward(passward!))){
+                        return 'not vaild';
+                      }
+                      else
+                        return 'vaild';
+                    },
+                    decoration: InputDecoration(
+                        label: Text("passward"),
+                        suffixIcon: Icon(Icons.remove_red_eye_sharp),
+                        hintText: "*************",
+                        prefixIcon: Icon(Icons.password),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.blueAccent,
+                            width: 3,
+                          ),
+
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.blueAccent,
+                            width: 3,
+                          ),
+                        )
+                    ),
+
+                  ),
+                  SizedBox(
+                    height:30,
+                  ),
+                  ElevatedButton(onPressed:(){
+                    if(formkey.currentState!.validate());
+                    Navigator.of(context).push(
+                      MaterialPageRoute(builder: (_)=> StackClass ()),
+                    );
+
+                  } , child:Text('Login') ),
+                ],
+              ),
+            )),
+      )
+      ),
+    );
+
+  }
+  bool checkemail(String email) {
+    return RegExp(r'^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$').hasMatch(email);
+  }
+  bool checkpassward(String passward)
+
+  {
+    String pattern =r"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$";
+    return RegExp(pattern).hasMatch(passward);
+  }
+}
---
+++ lib/stack.dart
@@ -0,0 +1,322 @@
+import 'package:flutter/material.dart';
+
+
+class StackClass extends StatefulWidget {
+  const StackClass({super.key});
+
+  @override
+  State<StackClass> createState() => _StackClassState();
+}
+
+class _StackClassState extends State<StackClass> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.pink[90],
+        title: Row(
+
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text("Aduit",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),),
+              Text("sign in",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ) ),
+              Icon(IconData(0xefa7, fontFamily: 'MaterialIcons')),
+
+
+            ]
+        ),
+      ),
+
+
+      body: Stack(
+          children: [
+            Column(
+              children: [
+                Expanded(
+                    flex: 4,
+                    child: Stack(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                              color: Colors.purple[100],
+                              borderRadius: BorderRadius.only(bottomLeft: Radius.circular(50))
+
+                          ),
+                        ),
+                        Column(
+                          children: [
+                            Padding(padding: const EdgeInsets.all(8.0),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: Colors.white,
+
+                                ),
+                                height: 60,
+                                width: 60,
+                                child: Icon(Icons.person),
+
+                              ),
+                            ),
+                            Padding(padding:const EdgeInsets.all(8.0) ,
+                                child:Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    box(Colors.purple),
+                                    box(Colors.grey),
+                                    box(Colors.grey),
+                                    box(Colors.grey),
+
+                                  ],
+                                )
+
+                            ),
+                          ],
+                        )
+                      ],)
+
+                ),
+                Expanded(flex: 1,
+                    child:
+                    Container(
+                      decoration: BoxDecoration(
+                          color: Colors.purple,
+                          borderRadius: BorderRadius.only(topLeft: Radius.circular(30),topRight: Radius.circular(30))
+                      ),
+                    )
+                ),
+
+              ],
+
+            ),
+            Positioned(
+                top: 180,
+                right: 80,
+                child:
+
+                Stack(
+                    children:[ Container(
+                      height: 300,
+                      decoration: BoxDecoration(
+                          shape: BoxShape.rectangle,
+                          color: Colors.white
+                      ),
+                      width: 200,
+                    ),
+                      Container(
+
+
+                        child: Positioned(
+                          top: 10
+                          ,
+
+                          right: 10,
+                          left: 10,
+                          child: Text("1",
+                            style: TextStyle(color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 30
+                            ) ,
+                          ),
+                        ),
+
+                      ),
+                      Container(
+
+
+                        child: Positioned(
+                          top: 10,
+
+                          right: 50,
+                          left: 90,
+                          child: Text("2",
+                            style: TextStyle(color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 30
+                            ) ,
+                          ),
+                        ),
+
+                      ),
+                      Container(
+
+
+                        child: Positioned(
+                          right: 0,
+                          left: 170,
+                          top: 10,
+                          child: Text("3",
+                            style: TextStyle(color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 30
+                            ) ,
+                          ),
+                        ),
+
+                      ),
+                      Container(
+
+
+                        child: Positioned(
+                          right: 10,
+                          left: 10,
+                          top: 100
+                          ,
+                          child: Text("4",
+                            style: TextStyle(color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 30
+                            ) ,
+                          ),
+                        ),
+
+                      ),
+                      Container(
+
+
+                        child: Positioned(
+                          top: 100,
+
+                          right: 50,
+                          left: 90,
+                          child: Text("5",
+                            style: TextStyle(color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 30
+                            ) ,
+                          ),
+                        ),
+
+                      ),
+                      Container(
+
+
+                        child: Positioned(
+                          right: 0,
+                          left: 170,
+                          top: 100,
+                          child: Text("6",
+                            style: TextStyle(color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 30
+                            ) ,
+                          ),
+                        ),
+
+                      ),
+                      Container(
+
+
+                        child: Positioned(
+                          right: 10,
+                          left: 10,
+                          top: 100
+                          ,
+                          child: Text("4",
+                            style: TextStyle(color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 30
+                            ) ,
+                          ),
+                        ),
+
+                      ),
+                      Container(
+
+
+                        child: Positioned(
+                          right: 10,
+                          left: 10,
+                          top: 200
+                          ,
+                          child: Text("7",
+                            style: TextStyle(color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 30
+                            ) ,
+                          ),
+                        ),
+
+                      ),
+                      Container(
+
+
+                        child: Positioned(
+                          top: 200,
+
+                          right: 50,
+                          left: 90,
+                          child: Text("8",
+                            style: TextStyle(color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 30
+                            ) ,
+                          ),
+                        ),
+
+                      ),
+                      Container(
+
+
+                        child: Positioned(
+                          right: 0,
+                          left: 170,
+                          top: 200,
+                          child: Text("9",
+                            style: TextStyle(color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 30
+                            ) ,
+                          ),
+                        ),
+
+                      ),
+                      Positioned(
+                        top: 270,
+                        left: 170,
+                        child: Container(
+                          child:
+                          Icon( IconData(0xe139, fontFamily: 'MaterialIcons')),
+                        ),
+                      )
+                    ]
+                )
+            ),
+
+
+
+          ]
+
+      ),
+
+
+    );
+  }
+}
+Widget box(Color c){
+  return Padding(
+    padding:const EdgeInsets.all(5),
+    child: Positioned(
+      right: 50,
+      left: 30,
+      child: Container(
+        height: 20,
+        width: 20,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: c,
+        ),
+      ),
+    ),
+  );
+
+}
+
